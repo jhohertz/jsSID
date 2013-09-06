313862d69602ab7d6c21780a842d55d2692b24ab
Joe Hohertz <joe@hohertz.org>
2013-09-06 17:49:06 -0400

    moved js files to a folder. added jsxcompressor as it may be useful

--- js/stream.js
+++ js/stream.js
@@ -94,3 +94,68 @@ Stream.loadRemoteFile = function (path, callback) {
 	fetch.send();
 }
 
+
+Stream.Base64DecodeEnumerator = function(input)
+{
+    this._input = input;
+    this._index = -1;
+    this._buffer = [];
+}
+
+Stream.Base64DecodeEnumerator.prototype =
+{
+    current: 64,
+
+    codex : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+    moveNext: function()
+    {
+        if (this._buffer.length > 0)
+        {
+            this.current = this._buffer.shift();
+            return true;
+        }
+        else if (this._index >= (this._input.length - 1))
+        {
+            this.current = 64;
+            return false;
+        }
+        else
+        {
+            var enc1 = this.codex.indexOf(this._input.charAt(++this._index));
+            var enc2 = this.codex.indexOf(this._input.charAt(++this._index));
+            var enc3 = this.codex.indexOf(this._input.charAt(++this._index));
+            var enc4 = this.codex.indexOf(this._input.charAt(++this._index));
+
+            var chr1 = (enc1 << 2) | (enc2 >> 4);
+            var chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            var chr3 = ((enc3 & 3) << 6) | enc4;
+
+            this.current = chr1;
+
+            if (enc3 != 64)
+                this._buffer.push(chr2);
+
+            if (enc4 != 64)
+                this._buffer.push(chr3);
+
+            return true;
+        }
+    }
+};
+
+Stream.Base64Decode = function(input) {
+
+        var output = []; 
+
+        var enumerator = new Stream.Base64DecodeEnumerator(input);
+        while (enumerator.moveNext())
+        {
+            var charCode = enumerator.current;
+            output.push(String.fromCharCode(charCode));
+        }
+
+        return output.join("");
+}
+
+
