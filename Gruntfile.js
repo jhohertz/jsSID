0c934b27738d915eba1176733f68a88b7b0409f9
Joe Hohertz <jhohertz@gmail.com>
2014-08-02 14:50:03 -0400

    more work @ a real packaging/build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,65 @@
+"use strict";
+
 module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
-  // Load grunt tasks automatically
-  require('load-grunt-tasks')(grunt);
- 
-  /*
-   *   Time how long grunt tasks take to run, this might be important when having complex builds that take forever.
-   *   For now just to show how fancy grunt is.
-   */
-  require('time-grunt')(grunt);
-
-  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      all: [ "js/jssid.*.js" ],
+      options: {
+        curly   : true,
+        eqeqeq  : true,
+        latedef : true,
+        noarg   : true,
+        noempty : true,
+        quotmark: "double",
+        undef   : true,
+        strict  : true,
+        trailing: true,
+        newcap  : false,
+        browser : true,
+        node    : true
+      }
+    },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        sourceMap: "js/jssid.js.map"
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      files: {
+        "js/jssid.js": [
+          "js/jsxcompressor.js",
+          "js/stream.js",
+          "js/jssid.core.js",
+          "js/jssid.tinysid.js",
+          "js/jssid.fastsid.js",
+          "js/jssid.resid.js",
+          "js/jssid.mos6510.js",
+          "js/jssid.sidplayer.js",
+          "js/jssid.dmpplayer.js",
+          "js/pico.dev.js"
+        ]
       }
-    }
+      //build: {
+      //  src: 'src/<%= pkg.name %>.js',
+      //  dest: 'build/<%= pkg.name %>.min.js'
+      //}
+    },
+    watch: {
+      src: {
+        files: [ "js/jssid.*.js" ],
+        tasks: [ "default" ]
+      }
+    },
+    clean: [ "js/jssid.js", "js/jssid.js.map" ]
   });
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+  grunt.registerTask('default', [ 'jshint', 'uglify']);
 }
+
